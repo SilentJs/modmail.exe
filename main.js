7e1889e5a92cdbd9c5e5e0c4efe56e5e9fc698f8
SilentJs <86762707+SilentJs@users.noreply.github.com>
2021-07-06 14:48:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ client.on('message' , (message)=>{
 });
 client.once('ready', () => {
 	console.log('The bot is online !!');
-    client.user.setActivity("the people ! || Modmail.exe v1.4", {type:'LISTENING'}).catch(console.error)
+    client.user.setActivity(":( my life sucks", {type:'LISTENING'}).catch(console.error)
     
 });
 // client.on("message", message => {
