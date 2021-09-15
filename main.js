2d1f4279a169d1da9676a325e7d6595050318bbe
SilentJs <86762707+SilentJs@users.noreply.github.com>
2021-09-15 14:48:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ client.on('message' , (message)=>{
 });
 client.once('ready', () => {
 	console.log('The bot is online !!');
-    client.user.setActivity("to shri ram vibes", {type:'LISTENING'}).catch(console.error)
+    client.user.setActivity("shri ram vibes", {type:'LISTENING'}).catch(console.error)
     
 });
 // client.on("message", message => {
